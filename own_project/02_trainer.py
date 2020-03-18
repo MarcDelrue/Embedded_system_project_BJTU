45a03dc6a00473060b3e221ce2a4664950e12fd2
MarcDelrue <marc_delrue@yahoo.fr>
2020-03-18 15:33:38 +0100

    trainer works with new architecture

--- own_project/02_trainer.py
+++ own_project/02_trainer.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 import os
 # Path for face image database
-path = 'dataset'
+path = 'dataset/Users/'
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 detector = cv2.CascadeClassifier("/home/pi/opencv/data/haarcascades/haarcascade_frontalface_default.xml");
 # function to get the images and label data
@@ -11,14 +11,17 @@ def getImagesAndLabels(path):
     imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
     faceSamples=[]
     ids = []
-    for imagePath in imagePaths:
-        PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
-        img_numpy = np.array(PIL_img,'uint8')
-        id = int(os.path.split(imagePath)[-1].split(".")[1])
-        faces = detector.detectMultiScale(img_numpy)
-        for (x,y,w,h) in faces:
-            faceSamples.append(img_numpy[y:y+h,x:x+w])
-            ids.append(id)
+    for subFolder in imagePaths:
+        newDir = [os.path.join(subFolder,f) for f in os.listdir(subFolder)]
+        for imagePath in newDir:
+            PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
+            img_numpy = np.array(PIL_img,'uint8')
+            id = int(os.path.split(imagePath)[-1].split(".")[0])
+            print (id)
+            faces = detector.detectMultiScale(img_numpy)
+            for (x,y,w,h) in faces:
+                faceSamples.append(img_numpy[y:y+h,x:x+w])
+                ids.append(id)
     return faceSamples,ids
 print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
 faces,ids = getImagesAndLabels(path)
